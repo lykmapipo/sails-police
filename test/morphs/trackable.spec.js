8404c4e9fd019ae7329bbed8ca984a3ca7943c1f
lykmapipo <lallyelias87@gmail.com>
2015-01-29 17:44:55 +0300

    implementing Trackable#track instance method

--- test/morphs/trackable.spec.js
+++ test/morphs/trackable.spec.js
@@ -1,8 +1,13 @@
 var expect = require('chai').expect;
 var faker = require('faker');
+var async = require('async');
+
+var previousIp = faker.internet.ip();
+var currentIp = faker.internet.ip();
+var email = faker.internet.email();
 
 describe('Trackable', function() {
-    
+
     it('should have trackable static flag', function(done) {
         expect(User.trackable).to.be.true;
         done();
@@ -15,5 +20,74 @@ describe('Trackable', function() {
         expect(User._attributes.lastSignInAt).to.exist;
         expect(User._attributes.lastSignInIpAddress).to.exist;
         done();
+    });
+
+    it('should be able to set trackable details', function(done) {
+
+        var trackable = User.new({
+            email: email,
+            password: faker.internet.password()
+        });
+
+        expect(trackable.track).to.exist;
+        expect(trackable).to.respondTo('track');
+
+        trackable
+            .track(previousIp, function(error, trackable) {
+                if (error) {
+                    done(error);
+                } else {
+
+                    expect(trackable.currentSignInAt).to.not.be.null;
+                    expect(trackable.currentSignInIpAddress).to.not.be.null;
+                    expect(trackable.currentSignInIpAddress).to.equal(previousIp);
+                    done();
+                }
+            });
+    });
+
+    it('should be able to update tracking details', function(done) {
+        var lastSignInAt;
+
+        async
+            .waterfall(
+                [
+                    function(next) {
+                        User.findOneByEmail(email, next)
+                    },
+                    function(trackable, next) {
+                        lastSignInAt = trackable.currentSignInAt;
+
+                        expect(trackable.signInCount).to.equal(1);
+                        expect(trackable.currentSignInAt).to.not.be.null;
+                        expect(trackable.currentSignInIpAddress).to.not.be.null;
+                        expect(trackable.currentSignInIpAddress).to.equal(previousIp);
+
+                        next(null, trackable);
+                    },
+                    function(trackable, next) {
+                        trackable.track(currentIp, next)
+                    }
+                ],
+                function(error, trackable) {
+                    if (error) {
+                        done(error);
+                    } else {
+                        expect(trackable.signInCount).to.equal(2);
+
+                        expect(trackable.lastSignInAt).to.not.be.null;
+                        expect(trackable.lastSignInAt.getTime())
+                            .to.equal(lastSignInAt.getTime());
+                            
+                        expect(trackable.lastSignInIpAddress).to.not.be.null;
+                        expect(trackable.lastSignInIpAddress).to.equal(previousIp);
+
+                        expect(trackable.currentSignInAt).to.not.be.null;
+                        expect(trackable.currentSignInIpAddress).to.not.be.null;
+                        expect(trackable.currentSignInIpAddress).to.equal(currentIp);
+
+                        done();
+                    }
+                });
     });
 });
