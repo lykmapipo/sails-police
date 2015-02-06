83cb9ee84d047e4ae4902d614d9bc46ccea3f348
lykmapipo <lallyelias87@gmail.com>
2015-02-06 14:31:16 +0300

    Initial http test specs

---
+++ test/http/signup.http.spec.js
@@ -0,0 +1,50 @@
+var request = require('supertest');
+var jsdom = require('jsdom');
+var fs = require("fs");
+var path = require('path');
+var jquery = fs.readFileSync(path.join(__dirname, "jquery.js"), "utf-8");
+var async = require('async');
+var expect = require('chai').expect;
+
+describe('AuthController#signup', function() {
+
+    it('should respond to http#get on /signup', function(done) {
+        request(sails.hooks.http.app)
+            .get('/signup')
+            .set('Accept', 'text/html')
+            .end(function(error, response) {
+                async
+                    .waterfall(
+                        [
+                            function(next) {
+                                if (error) {
+                                    next(error);
+                                } else {
+                                    next(null, response);
+                                }
+                            },
+                            function(response, next) {
+                                jsdom
+                                    .env({
+                                        html: response.text,
+                                        src: [jquery],
+                                        done: next
+                                    });
+                            }
+                        ],
+                        function(error, window) {
+                            if (error) {
+                                done(error);
+                            } else {
+                                var $ = window.$;
+
+                                expect($('form').attr('action')).to.be.equal('/signup');
+
+                                done()
+                            }
+                        });
+
+            });
+    });
+
+});
