c41c233ab2730b1004eb2aa44cd9e9274210ae19
lykmapipo <lallyelias87@gmail.com>
2015-03-05 14:27:51 +0300

    remove unnecessary frontend libraries

--- config/police.js
+++ config/police.js
@@ -1,3 +1,18 @@
+/**
+ * @description sails-police configurations
+ * @type {Object}
+ */
 module.exports.police = {
-
+	//routes configurations
+	//affect how routes.mixin
+	//work internal
+    routes: {
+    	//a path where all
+    	//sails-police routes will
+    	//be relative to
+    	//Example if you specify mount path
+    	//the /login route wi
+    	//i.e mountPath/login
+        mountPath: ''
+    }
 }
